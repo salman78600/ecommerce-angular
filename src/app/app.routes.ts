a39a234bb542587b075d153265908542a5dbfb24
Saad Salman <saadsalman@saads-mbp.speedport.ip>
2025-04-21 15:53:28 +0200

    modify routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductCartComponent } from './pages/products/product-cart/product-cart.component';
+import { CartComponent } from './pages/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+
+    path: '',
+    pathMatch: 'full',
+    component: ProductsComponent 
+},
+    {
+        path: 'cart',
+        component: CartComponent
+    }
+];
