547e93bae7f03f990c33b62f8a2139b9bfdd6d62
Saad Salman <saadsalman@saads-mbp.speedport.ip>
2025-04-21 15:55:33 +0200

    modify cart service file

--- src/app/services/cart-service.service.ts
+++ src/app/services/cart-service.service.ts
@@ -11,10 +11,19 @@ export class CartServiceService {
   addToCart(product: Product) {
 
       // add new product to cart
-      this.cart.set([
-        ...this.cart(),
-        product]);
-      console.log(this.cart());
+      if(this.cart().some(item => item.id === product.id)) {
+        alert("Already added in cart");
+      }else{
+        this.cart.set([
+          ...this.cart(),
+          product]);
+        console.log(this.cart());
+      }
   
+  }
+
+  removeFromCart(product: Product) {
+    // remove product from cart
+    this.cart.set(this.cart().filter(item => item.id !== product.id));
   }
 }
